da5113767b086f464b903d8509545551f9e790f6
Mohamed Alaa Oraby <mosasameh123@gmail.com>
2026-03-17 05:48:20 +0200

    update verify-email desgin

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,18 +3,7 @@
 @section('title', 'Verify Email')
 
 @section('content')
-<div class="breadcrumb-section breadcrumb-bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 offset-lg-2 text-center">
-                <div class="breadcrumb-text">
-                    <p>Secure Account Access</p>
-                    <h1>Verify Email</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 <div class="product-section mt-150 mb-150">
     <div class="container">
