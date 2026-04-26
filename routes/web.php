e481c7cf878330b5f73108011b1753689e6b301d
Orabii <mosasameh123@gmail.com>
2026-04-26 03:57:21 +0300

    Docker

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes(['verify' => true]);
 
-Route::middleware('auth')->prefix('email')->name('verification.')->group(function () {
-    Route::get('/verify', [VerificationController::class, 'show'])->name('notice');
-    Route::post('/verify/resend', [VerificationController::class, 'resend'])->name('resend');
-});
+// Route::middleware('auth')->prefix('email')->name('verification.')->group(function () {
+//     Route::get('/verify', [VerificationController::class, 'show'])->name('notice');
+//     Route::post('/verify/resend', [VerificationController::class, 'resend'])->name('resend');
+// });
 
 /*
 |--------------------------------------------------------------------------
